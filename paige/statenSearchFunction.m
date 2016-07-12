59aa022fb5a52c75e6fdba12097a540eace81d17
Paige Pruce <Paige Pruce>
2016-07-12 14:43:08 -0400

    Ammended dropdown menu output options

--- paige/statenSearchFunction.m
+++ paige/statenSearchFunction.m
@@ -1,12 +1,21 @@
 function [FDList] = statenSearchFunction( time )
 %Dummy function to input a time array of 1 or 2 datenums and output an
 %array of structures containing FDs
+timeRange = time(2) - time(1);
 
 
-display('Hooray! You searched by date! Good 4 u')
+if timeRange > 7
+display('Oooooh weeee! You searched for MORE than a week! Good 4 u')
 FDList = { 'Paige' 'Orange';...
            'Staten' 'Dog';...
            'Ryan' 'Kite'};
+else
+    display('Oooooh weeee! You searched for LESS than a week! Good 4 u')
+    FDList = { 'Pruce' 'Orange';...
+               'Longo' 'Dog';...
+               'McGuire' 'Kite'};
+end
+      
 
 % data = {'Apple';'Bob';'Cat'};
 % celldata = cellstr(data)
