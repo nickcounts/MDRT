c181fe928f94ce236d4e2c1f2f80db830ace418e
Nick Counts <nick.counts@gmail.com>
2016-07-14 13:57:53 -0400

    "added smart hiding/showing of event labels to handle cases where labels are off the x-limits"

--- reviewPlot/reviewRescaleAllTimelineEvents.m
+++ reviewPlot/reviewRescaleAllTimelineEvents.m
@@ -3,6 +3,9 @@ function reviewRescaleAllTimelineEvents( varargin )
 %
 % TODO: Add documentation
 %
+%   Major revision to toggle event label visibility based on parent figure
+%   toolbar "show/hide" togglebutton state.
+%
 % Counts 2016 VCSFA
 
 % Set default search location to the root graphics object
@@ -24,6 +27,22 @@ switch nargin
             
             % Extract figure number
             parentFigure = varargin{2}.Source.Parent.Parent.Number;
+            
+            c = varargin{1}.Parent.Children;
+            
+            for i = 1:numel(c)
+                if strcmpi(c(i).Type,'uitoggletool')
+                    calledFromButton = true;
+                    expectedVisible  = c(i).State;
+                    break
+                else
+                    calledFromButton = false;
+                    expectedVisible = [];
+                end
+            end
+            
+            
+            
         end
         
     otherwise
@@ -38,15 +57,82 @@ end
 
     lines   = findall(parentFigure,'Tag','vline');
     labels  = findall(parentFigure,'Tag','vlinetext');
+    
+    labelsOnAxis = [];
+    labelsOutsideAxis = [];
+    
 
     for i = 1:length(lines)
         YLim = get(get(lines(i), 'Parent'),'YLim');
+        XLim = lines(i).Parent.XLim;
+        
         set(lines(i),'YData',YLim);
 
         pos = get(labels(i),'Position');
         pos(2) = min(YLim) + abs(diff(YLim))*vscale;
         set(labels(i),'Position',pos)
+        
+
+        % Build a list of labels that are inside and outside the x axis
+        if isOutside(labels(i).Position(1), XLim(1), XLim(2) )
+            labelsOutsideAxis = vertcat(labelsOutsideAxis, labels(i));
+        else
+            labelsOnAxis = vertcat(labelsOnAxis, labels(i));
+        end
+        
+        % Toggle visibility using rules for each case:
+        fixOffAxisLabelVisibility(labelsOutsideAxis);
+        fixOnAxisLabelVisibility(labelsOnAxis);
 
     end
     
+end
+
+function state = getExpectedStateForLabel(label)
     
+    state = [];
+    
+    tbut = findobj(label.Parent.Parent.Children, 'type','uitoggletool');
+    state = tbut.State;
+
+end
+
+
+
+function fixOnAxisLabelVisibility(label)
+    for i = 1:numel(label)
+        
+        % get expected label state based on toggle button
+        state = getExpectedStateForLabel(label(i)); 
+        
+        % change labels inside the axis to their expected state
+        label(i).Visible = state;
+
+    end
+
+end
+
+
+function fixOffAxisLabelVisibility(label)
+    for i = 1:numel(label)
+        label(i).Visible = 'off';
+    end
+
+end
+
+
+
+
+function r = isOutside(x, low,hi)
+   % returns logical true if low < x < hi
+
+   r = ~(x>low & x<hi);
+   
+end
+
+function r = isWithin(x, low,hi)
+   % returns logical true if low < x < hi
+
+   r = x>low & x<hi;
+   
+end
