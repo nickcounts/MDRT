019310197dfa83640e93a03c20a0f202c5381af9
Nick Counts <nick.counts@gmail.com>
2016-07-13 22:08:50 -0400

    "now loads metadata structures with error checking and calls the stringMaking function"

--- makeDataSetTitleStringFromActiveConfig.m
+++ makeDataSetTitleStringFromActiveConfig.m
@@ -1,4 +1,4 @@
-function [ titleString ] = makeDataSetTitleStringFromActiveConfig( input_args )
+function [ titleString ] = makeDataSetTitleStringFromActiveConfig( varargin )
 %makeDataSetTitleStringFromActiveConfig Summary of this function goes here
 %   makeDataSetTitleStringFromActiveConfig( configStruct )
 %   makeDataSetTitleStringFromActiveConfig( metadataStruct )
@@ -8,32 +8,29 @@ function [ titleString ] = makeDataSetTitleStringFromActiveConfig( input_args )
 
 titleString = '';
 
+dbug = true;
+
+
+
 switch nargin
     case 1 % Caller passed either a config struct or a metadata struct
         
-        switch checkStructureType(varargin{1}
+        switch checkStructureType( varargin{1} )
             case 'metadata'
                 % it was a metadata struct
+                
+                disp('passed a metadata struct')
                 
                 
             case 'config'
                 % it was a config struct
-                
                 % load the metadata struct
                 
-                filename = fullfile(config.dataFolderPath,'metadata.mat');
+                config = varargin{1};
                 
-                s = whos('-file', filename);
+                metaData = loadMetadataFromConfig(config);
                 
-                if strcmp('metaData', s.name ) && strcmp('struct', s.class)
-                    % Pretty confident we have the real deal here
-                    load(filename);
-                else
-                   % If I failed this test, bad shit is happeneing.
-                   % Soft fail by returning empty string
-                   return
-                end
-                
+                disp('passed a config struct, loaded metadata')
                 
             otherwise
                 % User passed a bogus/unsupported variable type
@@ -43,21 +40,70 @@ switch nargin
         end
         
         
-        
-        
-        % Check to see if it was a config struct and load the metadata
-        % struct first, then behave
-        
-        
     case 0
         % Caller was a jerk and did give you anything!
         % Try to load config, grab metadata and then do the work
+        
+        disp('you''re a jerk')
+        
+        metaData = loadMetadataFromConfig(getConfig);
+        
+        disp('but it worked out')
+        
     otherwise
         % If it's more than 1 variable passed I don't know what happened?
         % Soft fail and return empty set
+        return
+end
+
+% If you made it this far, there is a valid metaData variable/struct in
+% memory
+
+titleString = makeStringFromMetaData(metaData);
+
+end
+
+function metaData = loadMetadataFromConfig(config)
+
+filename = fullfile(config.dataFolderPath,'metadata.mat');
+                
+    if exist(filename,'file') == 2 % File exists
+
+        % Test that the file contains the right variable
+        s = whos('-file', filename);
+
+        if strcmp('metaData', s.name ) && strcmp('struct', s.class)
+            % Pretty confident we have the real deal here
+            load(filename);
+            % now metaData is loaded. Should I do this explicitely?
+        else
+           % If I failed this test, bad shit is happeneing.
+           % Soft fail by returning empty string
+           disp('passed a config struct, no metadata found')
+           return
+        end
+        
+
+    else % no metadata file found
+        
+        disp('passed a config struct, metadata file missing')
+        % Soft fail by returning empty string
+        return
+    end
+                
+
 end
 
 
 
+function titleString = makeStringFromMetaData(metaData)
+
+    titleString = '';
+
+    
+    
+    
 end
+
+
 
