8e9b1d14b6c73697020708f6e4c356f9dd92c853
StatenLongo <slongo@outlook.com>
2016-07-19 10:56:14 -0400

    working bugs in searchTimeStamp

--- staten/dummySearchTimeStamp.m
+++ staten/dummySearchTimeStamp.m
@@ -1,6 +1,9 @@
-datenum1 = datenum('05-01-2016')
-datenum2 = datenum('06-30-2016')
-hotfire = datenum('05-31-2016')
+% datenum1 = datenum('05-01-2016')
+% datenum2 = datenum('06-30-2016')
+hotfire = datenum('05-31-2016');
 
-searchTimeStamp(['31-May-2016'])
+ORB1 = 735610;
+%hotfire = 736475;
+
+searchTimeStamp([ORB1 hotfire])
 
--- staten/searchTimeStamp.m
+++ staten/searchTimeStamp.m
@@ -32,6 +32,7 @@ function [foundDataToSearch] = searchTimeStamp( timeStamp )
 	
 %% above is good for a single instance, but need this to look at dataIndexForSearching, which will return a file in data archive folder to be searched
 
+% ========================================================================
 
 % timelength = length(timestamp);
 % 
@@ -45,100 +46,155 @@ function [foundDataToSearch] = searchTimeStamp( timeStamp )
 %                     
 %     end
 
+% =========================================================================
+
+% dataRepositoryDirectory = 'C:\Users\Staten\Desktop\Data Repository'; % set file path
+% 
+% load( fullfile(dataRepositoryDirectory, 'dataToSearch.mat') ); % load variables from dataToSearch
+% 
+% timeSpanInput = length( timeStamp ); % is input 1 or 2 values?
+% 
+% for i = 1:length(dataToSearch) % index dataToSearch for every structure
+%     
+%     timeSpan = dataToSearch(i).metaData.timeSpan
+%     % ^ can I do this to clean up the code?/ does this work the way I think
+%     % it will or will I end up writing over the existing file on accident
+%     
+%     switch timeSpanInput
+%         
+%         case 1 % input is 1 value, iterate over 24 hour single day period
+%             % this does not work WHY WON'T THIS WORK
+%             % I don't think duration does what I think it does/ want it to
+%             % do... better matlab function for this?
+%             %DURATION IS POO USE INBETWEEN
+%             singleDay = duration(timeSpanInput) % sets 24 hour period to iterate over
+%             
+%             % index to iterate over the metaData timeSpan
+%             for j = dataToSearch(i).metaData.timeSpan(1):dataToSearch(i).metaData.timeSpan(2)
+%                 % if the 24 hour single day period occurs within a metaData
+%                 % timeSpan
+%                 if timestamp == dataToSearch(i).metaData.timeSpan(j)
+%                 
+%                     % return those dataToSearch structures
+%                     dataToSearch
+%                     break
+%                 end
+%             end
+%             
+%                     % also return list of fds over this timespan?
+%             
+%         case 2 % input is 2 values, iterate over period between 2 days given
+%             
+%             % index to iterate over the metaData timeSpan 
+%             for j = dataToSearch(i).metaData.timeSpan(1):dataToSearch(i).metaData.timeSpan(2)
+%                 
+%                 % index to iterate over the timeSpanInput given by the GUI
+%                 for k = duration( timeSpanInput(1) ):duration( timeSpanInput(2) )
+%                     
+%                     % if the timeSpanInput given by the GUI is contained
+%                     % within OR overlapping ( <- did I accomodate for this?)
+%                     if duration( timeSpanInput(k) ) == dataToSearch(i).metaData.timeSpan(j)
+%                         
+%                         % return those dataToSearch structures
+%                         dataToSearch
+%                         break
+%                     end
+%                 end
+%             end
+%             
+%                         % also return list of fds over this timespan?
+%     end
+% end
+% end
+
+% ========================================================================
+
+% dataRepositoryDirectory = 'C:\Users\Staten\Desktop\Data Repository'; % set file path
+% 
+% load( fullfile(dataRepositoryDirectory, 'dataToSearch.mat') ); % load variables from dataToSearch
+% 
+% timeSpanInput = length( timeStamp ); % is input 1 or 2 values?
+% 
+% foundDataToSearch = {}; % empty cell array of structures - will hold list
+% 
+% 
+% for i = 1:length(dataToSearch)
+%         
+%         
+%     switch timeSpanInput
+% 
+%         case 1 % input is 1 value, iterate over 24 hour single day period
+% 
+%             timeStampIsValid = isbetween( timeStamp, datetime( dataToSearch(i).metaData.timeSpan(1), 'ConvertFrom', 'datenum' ), datetime( dataToSearch(i).metaData.timeSpan(2), 'ConvertFrom', 'datenum' ) )
+%             
+%             timeStampIsValid
+%             
+%             if timeStampIsValid == 1
+% 
+%                 foundDataToSearch{length(foundDataToSearch)+1} = dataToSearch(i);
+%             end
+%    
+%         case 2 % input is 2 values, iterate over period between 2 days given
+% 
+% 
+%     end
+% end
+% end
+
+
+% ========================================================================4
+
 
 dataRepositoryDirectory = 'C:\Users\Staten\Desktop\Data Repository'; % set file path
 
 load( fullfile(dataRepositoryDirectory, 'dataToSearch.mat') ); % load variables from dataToSearch
 
-timeSpanInput = length( timeStamp ); % is input 1 or 2 values?
-
-for i = 1:length(dataToSearch) % index dataToSearch for every structure
-    
-    timeSpan = dataToSearch(i).metaData.timeSpan
-    % ^ can I do this to clean up the code?/ does this work the way I think
-    % it will or will I end up writing over the existing file on accident
-    
-    switch timeSpanInput
-        
-        case 1 % input is 1 value, iterate over 24 hour single day period
-            % this does not work WHY WON'T THIS WORK
-            % I don't think duration does what I think it does/ want it to
-            % do... better matlab function for this?
-            %DURATION IS POO USE INBETWEEN
-            singleDay = duration(timeSpanInput) % sets 24 hour period to iterate over
-            
-            % index to iterate over the metaData timeSpan
-            for j = dataToSearch(i).metaData.timeSpan(1):dataToSearch(i).metaData.timeSpan(2)
-                % if the 24 hour single day period occurs within a metaData
-                % timeSpan
-                if timestamp == dataToSearch(i).metaData.timeSpan(j)
-                
-                    % return those dataToSearch structures
-                    dataToSearch
-                    break
-                end
-            end
-            
-                    % also return list of fds over this timespan?
-            
-        case 2 % input is 2 values, iterate over period between 2 days given
-            
-            % index to iterate over the metaData timeSpan 
-            for j = dataToSearch(i).metaData.timeSpan(1):dataToSearch(i).metaData.timeSpan(2)
-                
-                % index to iterate over the timeSpanInput given by the GUI
-                for k = duration( timeSpanInput(1) ):duration( timeSpanInput(2) )
-                    
-                    % if the timeSpanInput given by the GUI is contained
-                    % within OR overlapping ( <- did I accomodate for this?)
-                    if duration( timeSpanInput(k) ) == dataToSearch(i).metaData.timeSpan(j)
-                        
-                        % return those dataToSearch structures
-                        dataToSearch
-                        break
-                    end
-                end
-            end
-            
-                        % also return list of fds over this timespan?
-    end
-end
-end
-
-dataRepositoryDirectory = 'C:\Users\Staten\Desktop\Data Repository'; % set file path
-
-load( fullfile(dataRepositoryDirectory, 'dataToSearch.mat') ); % load variables from dataToSearch
 
 timeSpanInput = length( timeStamp ); % is input 1 or 2 values?
 
-foundDataToSearch = {}; % empty cell array of structures - will hold list
+foundDataToSearch = newSearchResult; % empty cell array of structures from Paige - will hold list
 
+% 
+%keyboard
 
 for i = 1:length(dataToSearch)
-        
-        
+
     switch timeSpanInput
 
         case 1 % input is 1 value, iterate over 24 hour single day period
 
-            timeStampIsValid = isbetween( timeStamp, datetime( dataToSearch(i).metaData.timeSpan(1), 'ConvertFrom', 'datenum' ), datetime( dataToSearch(i).metaData.timeSpan(2), 'ConvertFrom', 'datenum' ) )
+            timeStamp(2) = timeStamp(1) + 1; % creates a second timestamp input
             
-            timeStampIsValid
-            
-            if timeStampIsValid == 1
+            if timeStamp(1) >= dataToSearch(i).metaData.timeSpan(1) && timeStamp(2) <= dataToSearch(i).metaData.timeSpan(2)
 
-                foundDataToSearch{length(foundDataToSearch)+1} = dataToSearch(i);
+                % foundDataToSearch{length(foundDataToSearch)+1} = dataToSearch(i);
+               
+                foundDataToSearch.metaData = dataToSearch(i).metaData;
+                foundDataToSearch.pathToData = dataToSearch(i).pathToData;
+                
+                i = i + 1;
+                
             end
-   
+
         case 2 % input is 2 values, iterate over period between 2 days given
-
-
+            if timeStamp(1) >= dataToSearch(i).metaData.timeSpan(1)
+                foundDataToSearch.metaData = dataToSearch(i).metaData;
+                foundDataToSearch.pathToData = dataToSearch(i).pathToData;
+            elseif timeStamp(2) <= dataToSearch(i).metaData.timeSpan(2)
+                % foundDataToSearch{length(foundDataToSearch)+1} = dataToSearch(i);
+     
+                foundDataToSearch.metaData = dataToSearch(i).metaData;
+                foundDataToSearch.pathToData = dataToSearch(i).pathToData;
+                
+                i = i + 1;
+            end
+           
+        otherwise 
+            disp('not working....')
     end
 end
 end
 
-            
-                
     
     
 %% subfunctions?
