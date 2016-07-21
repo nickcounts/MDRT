d1a827ec49230b56381cf35d5b2330b1f6e873f6
Nick Counts <nick.counts@gmail.com>
2016-07-21 18:32:50 -0400

    "added deletion for lists tagged 'op1FDlist' or 'op2FDlist'. added documentation. fixed indentation for readability."

--- comparison/fdListClickCallback.m
+++ comparison/fdListClickCallback.m
@@ -1,12 +1,78 @@
 function fdListClickCallback(hListbox, eventData, varargin)
+%fdListClickCallback
+%
+% fdListClickCallback( listboxHandle, ~, ~)
+%
+% Will move the double-clicked list item from a list tagged
+% 'listSearchResults' to the any list whos handle is stored in appdata as
+% 'targetOpFDList'
+%
+% Will delete an item from a list tagged 'op1FDlist' or 'op2FDlist' if
+% double clicked.
+%
+% No single-click behavior as yet.
+%
+% Counts, VCSFA 2016
+
+
     persistent lastTime
+    
     ONE_SEC = 1/(60*60*24);
+    
     if isempty(lastTime) || now-lastTime > 0.3*ONE_SEC
         lastTime = now;
-        % process the event (single-click)
+        % process a single-click
         
     else
-        % double-click
+        % Process a double-click
+        
+        
+        switch hListbox.Tag
+            case {'op1FDlist', 'op2FDlist'}
+                
+                i = hListbox.Value;
+                fdl = hListbox.String;
+                
+                % Basic deletion of list string
+                if isempty(fdl)
+                    fdl = {};
+                else
+                    fdl(i) = [];
+                end
+                
+                % If you delete at the end of a list, reset the selection
+                if i > length(fdl)
+                    hListbox.Value = length(fdl);
+                elseif isempty(fdl)
+                    % Handle empty list
+                    hListbox.Value = 1;
+                end
+                
+                hListbox.String = fdl;
+                
+            case {'listSearchResults'}
+                % Function originated in the search results list
+                              
+                mdrt = getappdata(hListbox.Parent);
+
+                % Get the contents of the target list
+                tfdl = cellstr(mdrt.targetOpFDList.String);
+
+                % Remove any blank cells left over from the initial list
+                tfdl = tfdl(~cellfun('isempty',tfdl));
+
+                % Get the selected FD String
+                fds = cellstr(hListbox.String{ hListbox.Value });
+
+                % Find match in masterFDList ?
+
+                % Add selected FD String to the target list
+                mdrt.targetOpFDList.String = vertcat(tfdl, fds);
+                
+            otherwise
+                % Nothing else I can think of for now
+        end
         
     end
+    
 end
