050abfc34f977083bed066a1da438bd33def1469
Nick Counts <nick.counts@gmail.com>
2016-07-21 16:31:28 -0400

    "now updates the events list and stores event times in appdata"

--- comparison/updateDataSelectionPopup.m
+++ comparison/updateDataSelectionPopup.m
@@ -1,7 +1,13 @@
-function updateDataSelectionPopup( ~, ~, htarget, hsource1, hsource2 )
+function updateDataSelectionPopup( pUp, ~ )
 %updateDataSelectionPopup 
 %
 % pass handle of updateTarget, source 1 and source 2
+
+
+hsource1 = findobj(pUp.Parent, 'Tag', 'opList1');
+hsource2 = findobj(pUp.Parent, 'Tag', 'opList2');
+htarget = findobj(pUp.Parent, 'Tag', 'selectDataList');
+
 
     % Get selection from top/bottom plot data set popup
     dataSetSelections = { hsource1.String{hsource1.Value}  ;
@@ -14,6 +20,14 @@ function updateDataSelectionPopup( ~, ~, htarget, hsource1, hsource2 )
     end
 
     htarget.String = dataSetSelections;
+    
+    
+    % update the contents of event popups on data set selection
+    [el1, el2] = updateEventDropdowns(pUp);
+    
+    % update appdata to store eventTimes
+    setappdata(pUp.Parent,'eventTimes1', el1);
+    setappdata(pUp.Parent,'eventTimes2', el2);
 
 end
 
