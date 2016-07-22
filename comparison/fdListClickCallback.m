0357dcb3e1c8cdaaafef013bf36709550f4b10ed
Nick Counts <nick.counts@gmail.com>
2016-07-22 03:05:59 -0400

    ""

--- comparison/fdListClickCallback.m
+++ comparison/fdListClickCallback.m
@@ -30,14 +30,27 @@ function fdListClickCallback(hListbox, eventData, varargin)
         switch hListbox.Tag
             case {'op1FDlist', 'op2FDlist'}
                 
+                switch(hListbox.Tag)
+                    case 'op1FDlist'
+                        debugout('loaded topPlot fd list')
+                        plotList = getappdata(hListbox.Parent, 'topPlot');
+                    case 'op2FDlist'
+                        debugout('loaded botPlot fd list')
+                        plotList = getappdata(hListbox.Parent, 'botPlot');
+                end
+                            
+                    
+                
                 i = hListbox.Value;
                 fdl = hListbox.String;
                 
                 % Basic deletion of list string
                 if isempty(fdl)
                     fdl = {};
+                    plotList = {};
                 else
                     fdl(i) = [];
+                    plotList(i,:) = [];
                 end
                 
                 % If you delete at the end of a list, reset the selection
@@ -45,29 +58,80 @@ function fdListClickCallback(hListbox, eventData, varargin)
                     hListbox.Value = length(fdl);
                 elseif isempty(fdl)
                     % Handle empty list
+                    debugout('plot fd list is empty')
                     hListbox.Value = 1;
                 end
                 
                 hListbox.String = fdl;
                 
+                % update appdata
+                switch(hListbox.Tag)
+                    case 'op1FDlist'
+                        setappdata(hListbox.Parent, 'topPlot', plotList);
+                    case 'op2FDlist'
+                        setappdata(hListbox.Parent, 'botPlot', plotList);
+                end
+                
             case {'listSearchResults'}
                 % Function originated in the search results list
                               
                 mdrt = getappdata(hListbox.Parent);
+                
+                % Fix for weird bug where matlab sets listbox Value=[] when
+                % clicking inside an empty listbox
+                if isempty(mdrt.targetOpFDList.Value) || mdrt.targetOpFDList.Value == 0
+                    mdrt.targetOpFDList.Value = 1;
+                end
+                
+                % get the appdata with the target list contents
+                switch mdrt.targetOpFDList.Tag
+                    case 'op1FDlist'
+                        listData = mdrt.topPlot;
+                    case 'op2FDlist'
+                        listData = mdrt.botPlot;
+                    otherwise
+                        
+                end
+                
+                
 
                 % Get the contents of the target list
-                tfdl = cellstr(mdrt.targetOpFDList.String);
+%                 tfdl = cellstr(mdrt.targetOpFDList.String);
+                if isempty(listData)
+                    tfdl = {};
+                else
+                    tfdl = listData(:,1);
+                end
 
                 % Remove any blank cells left over from the initial list
                 tfdl = tfdl(~cellfun('isempty',tfdl));
 
                 % Get the selected FD String
                 fds = cellstr(hListbox.String{ hListbox.Value });
+                
+                
 
                 % Find match in masterFDList ?
+                
+                
+                cellfind = @(string)(@(cell_contents)(strcmp(string,cell_contents)));
+                fdIndex = cellfun(cellfind(fds),mdrt.fdMasterList(:,1));
+                
+                newListDataRow = mdrt.fdMasterList(fdIndex,:);
+                
 
                 % Add selected FD String to the target list
                 mdrt.targetOpFDList.String = vertcat(tfdl, fds);
+                
+                % Add the newListDataRow to the appdata
+                switch mdrt.targetOpFDList.Tag
+                    case 'op1FDlist'
+                        setappdata(hListbox.Parent, 'topPlot', vertcat(listData, newListDataRow));
+                    case 'op2FDlist'
+                        setappdata(hListbox.Parent, 'botPlot', vertcat(listData, newListDataRow));
+                    otherwise
+                        
+                end
                 
             otherwise
                 % Nothing else I can think of for now
