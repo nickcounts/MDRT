6f4d1b4b3b25dc221f11385187671a772a1412a2
Nick Counts <nick.counts@gmail.com>
2016-08-24 18:09:42 -0400

    "updated with MDRTConfig object - automatically loads variables for testing"

--- class/makeTestListbox.m
+++ class/makeTestListbox.m
@@ -5,8 +5,10 @@ hs.editbox = uicontrol(hs.fig, 'style','edit','Units','normalized','Position',[0
 % hs.listbox.Units = 'normalized'
 % hs.listbox.Position = [0.1,0.1,0.8,0.8]
 
+cfg = MDRTConfig;
+load(fullfile(cfg.dataArchivePath, 'dataIndex.mat'));
 
-fdc = FDCollection(dataIndex(1), hs.listbox);
+fdc = FDCollection(dataIndex(1:2), hs.listbox);
 fdc.searchUI = hs.editbox;
 fdc.populateListbox;
 
