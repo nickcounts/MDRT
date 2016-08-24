e4e6244a7f070b38a802876d89c6096874040996
Nick Counts <nick.counts@gmail.com>
2016-08-24 05:56:55 -0400

    "updated to link searchbox and listbox"

--- class/makeTestListbox.m
+++ class/makeTestListbox.m
@@ -1,8 +1,12 @@
 hs.fig = figure;
 hs.listbox = uicontrol(hs.fig, 'style','listbox','Units','normalized','Position',[0.1,0.1,0.8,0.8]);
+hs.editbox = uicontrol(hs.fig, 'style','edit','Units','normalized','Position',[0.1,0.95,0.8,0.05],'HorizontalAlignment','left');
 
 % hs.listbox.Units = 'normalized'
 % hs.listbox.Position = [0.1,0.1,0.8,0.8]
 
 
+fdc = FDCollection(dataIndex(1), hs.listbox);
+fdc.searchUI = hs.editbox;
+fdc.populateListbox;
 
