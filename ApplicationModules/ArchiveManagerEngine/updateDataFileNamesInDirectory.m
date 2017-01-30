004fe6366054b3e5d69d335ff1d1437d29176ac1
Nick Counts <nick.counts@gmail.com>
2017-01-30 15:25:12 -0500

    "merged"

--- ApplicationModules/ArchiveManagerEngine/updateDataFileNamesInDirectory.m
+++ ApplicationModules/ArchiveManagerEngine/updateDataFileNamesInDirectory.m
@@ -37,6 +37,7 @@ originalFiles(~cellfun(@isempty, regexpi(originalFiles, 'metadata.mat'))) = [];
 originalFiles(~cellfun(@isempty, regexpi(originalFiles, 'timeline.mat'))) = [];
 originalFiles(~cellfun(@isempty, regexpi(originalFiles, 'AvailableFDs.mat'))) = [];
 
+% AvailableFDs
 
 %% Make temporary directories
 
