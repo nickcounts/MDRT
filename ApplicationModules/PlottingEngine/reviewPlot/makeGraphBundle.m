7cabcd0af143b9fce5030c528cc2529e334cecb1
Trisha Patel <trisha23@gmail.com>
2017-08-07 12:55:16 -0400

    Updated titles and annotations
    Changed window titles and added annotations

--- ApplicationModules/PlottingEngine/reviewPlot/makeGraphBundle.m
+++ ApplicationModules/PlottingEngine/reviewPlot/makeGraphBundle.m
@@ -1,5 +1,15 @@
 function makeGraphBundle ( varargin )
+% makeGraphBundle ( varargin )
+%   Conctanates multiple graph structures into a single structure. The
+%   combined structure can then be submitted to 
+%   VARARGIN - currently undefined or unused
+
+%   This tool serves as part of report generation development. It
+%   simplifies and concentrates the need for combining graphs and
+%   submitting them to plot. Structures themselves must be created in the
+%   plot setup tool.
 %
+% August 2017, Patel - Mid-Atlantic Regional Spaceport
 
 %% GUI Setup
 
@@ -8,11 +18,14 @@ function makeGraphBundle ( varargin )
     guiSize = [125 28];
     r.fig.Units = 'characters';
     r.fig.Position = [75 20 guiSize];
-    r.fig.Name = 'Report Generation Beta';
+    r.fig.Name = 'Combine Graph Structures';
     r.fig.NumberTitle = 'off';
     r.fig.MenuBar = 'none';
    
 %% GUI Layout
+
+% The gui uicontrol is set up within the function, and the function is
+% called later. This is just to make the code seem less cluttered.
 
     function guiOpeningFcn( varargin )
         r.availableText = uicontrol(r.fig, ...
@@ -109,22 +122,29 @@ function makeGraphBundle ( varargin )
 
 guiOpeningFcn()
 handles = guihandles(r.fig); 
-handles.pathnames = {}; 
+handles.pathnames = {}; % Create empty handles fields to be populated in the callbacks
 handles.filenames = {};
 handles.graph = [];
 
 guidata(r.fig, handles);
 
 %% Variable Definitions
-    plotFlag = false;
-    config = getConfig;
+
+    plotFlag = false; % flag for checking if graph structure combined properly
+    config = getConfig; % pulls configuration path files
 
 %% Callback Functions
     
     function browseCallback( hObject, events, handles )
         handles = guidata(hObject);
+        
+        % User chooses the folder holding graph configs
+        % You can switch browse folders after initial selection
         handles.path = uigetdir;
         handles.folder = fullfile(handles.path, '*.gcf');
+        
+        % The files and the file names are stored to the guidata and
+        % updates the listbox string. 
         handles.files = dir(handles.folder); 
         fileList = string(cellstr({handles.files.name}')); 
         
@@ -138,6 +158,7 @@ guidata(r.fig, handles);
     function clearCallback( hObject, events, handles )
         handles = guidata(hObject);
         
+        % Clears the listbox and all associated stored handles
         set(handles.select,'String','');
         handles.pathnames = {};
         handles.filenames = {};
@@ -151,17 +172,23 @@ guidata(r.fig, handles);
         
         switch hObject.Tag
             case 'add'
+                
+                % Pull index of highlighted selection and pull associated
+                % path and filename.
                 index = handles.contents.Value; 
                 selFileName = char(handles.contents.String(index,:));
                 selPath = fullfile(handles.path,selFileName);
                 
+                % Store selection to guidata and create a list.
                 handles.pathnames{end+1} = selPath;
                 handles.filenames{end+1} = selFileName;
+                
+                % Update the listbox selection 
                 handles.select.String = handles.filenames;
                 
-                if index == 1 %% is not a number?
-                    set(handles.contents,'Value',1);
-                end
+%                 if index == 1 %% is not a number?
+%                     set(handles.contents,'Value',1);
+%                 end
                 
                 drawnow
                 
@@ -170,10 +197,15 @@ guidata(r.fig, handles);
             case 'delete'
                 index = handles.select.Value;
                 
+                % Delete index file name and path based on selection.
+                % Update the listbox string. 
                 handles.pathnames(index) = [];
                 handles.filenames(index) = [];
+                
                 handles.select.String = handles.filenames;
                 
+                % Reselect the next listbox value right above the deleted
+                % object. 
                 if index == 1
                     set(handles.contents,'Value',1);
                 else
@@ -190,6 +222,8 @@ guidata(r.fig, handles);
     function generateCallback( hObject, events, handles )
         handles = guidata(hObject)
         
+        % Load the files based on the pathname and concatenate it to a
+        % graph structure. 
         for i = 1:length(handles.pathnames)
             dummy = load(handles.pathnames{i},'-mat');
             if i == 1
@@ -199,18 +233,19 @@ guidata(r.fig, handles);
             end
         end   
         
-        % check
+        % Check if the graph structure length and # of paths match up 
         if length(handles.pathnames) == length(graph)
            plotFlag = true; 
-           handles.graph = graph;
+           handles.graph = graph; % store to guidata
            guidata(hObject, handles)
         end
         
         if plotFlag
+            % Pull the inputted string for the file name
             structName = handles.name.String; 
             
-            % clean up unhappy reserved filename characters (code borrowed
-            % from makeGraphGUI)
+            % Following Code pulled from Counts' makeGraphGUI
+            % clean up unhappy reserved filename characters
             structName = regexprep(structName,'^[!@$^&*~?.|/[]<>\`";#()]','');
             structName = regexprep(structName, '[:]','-');
 
@@ -238,12 +273,18 @@ guidata(r.fig, handles);
     function plotCallback ( hObject, events, handles )
         handles = guidata(hObject);
         
+        % If there is no graph structure already in the guidata, open and
+        % load a new one to plot. 
         if isempty(handles.graph)
             [file, path] = uigetfile('.gcf','Select Graph Structure');
             input = load(fullfile(path, file),'-mat');
             input = input.graph;
+            
             plotGraphFromGUI(input);
+            
         else
+            % If the plotting command is executed with a graph structure
+            % present, that structure is plotted. 
             plotGraphFromGUI(handles.graph)
         end
         
