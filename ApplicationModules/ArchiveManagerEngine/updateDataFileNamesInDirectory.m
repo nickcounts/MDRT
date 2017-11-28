d871d636d4f18cb5676445b517ddeab540109a08
Nick Counts <nick.counts@gmail.com>
2017-11-28 14:52:18 -0500

    New Feature: Splits old style combined valve data into separate files
    Looks for FD structs that contain the 'position' timeseries and breaks them into 'State' and 'Position' FDs and data files.
    
     * Added debugging console output
     * Added documentation

--- ApplicationModules/ArchiveManagerEngine/updateDataFileNamesInDirectory.m
+++ ApplicationModules/ArchiveManagerEngine/updateDataFileNamesInDirectory.m
@@ -6,6 +6,14 @@ function updateDataFileNamesInDirectory(pathToData)
 % If no path is supplied, prompts user for directory, backs up all data 
 % files, renames using the 'makeFileNameForFD' function. Places problem 
 % data files in an 'error' directory for visibility.
+%
+% This function also converts old-style valve data files from combined
+% position and status to separate data files for State and Position Mon
+%
+% pathToData is a full system path string to the directory where the data
+% files are (normally called 'data') and not the higher level folder,
+% typically named by the data and description.
+
 
 % Find all data files in directory
 
@@ -23,7 +31,9 @@ else
         error('input argument is not a valid directory');
     end
 end
-    
+
+debugout('Processing data files in the folder:')
+debugout(rootDir_path)
     
 directoryList = dir(rootDir_path);
 
@@ -51,6 +61,8 @@ mkdir(backupDir_path);
 mkdir(issuesDir_path);
 mkdir(errorDir_path);
 
+debugout('Created subdirectories for safe processing')
+
 % Prepare UI Progress Indicator
 numberOfFiles = numel(originalFiles);
 totalLoops = numberOfFiles * 3;
@@ -61,6 +73,9 @@ progressbar('Data Directory Update - Overall', ...
             'Updating Data File Names');
 
 %% Copy entire rootPath into backupDir
+
+debugout('Backing up original data files')
+
 for i = 1:numberOfFiles
     copyfile(fullfile(rootDir_path, originalFiles{i}), backupDir_path);
     totalFrac = i/totalLoops;
@@ -68,6 +83,9 @@ for i = 1:numberOfFiles
 end
 
 %% Move entire rootPath into tempDir (the working directory)
+
+debugout('Moving data files to working directory')
+
 for i = 1:numberOfFiles
     movefile(fullfile(rootDir_path, originalFiles{i}), tempDir_path);
     totalFrac = (numberOfFiles + i)/totalLoops;
@@ -76,18 +94,27 @@ end
 
 %% Open each file, and build a new name based on fd.FullString
 
+debugout('Looping through each data file')
+
 for i = 1:numberOfFiles
     
     % Select a file on which to operate:
     thisFullFile = fullfile(tempDir_path, originalFiles{i});
+    
+    debugout(sprintf('Processing file %s', thisFullFile))
 
     % Error handling for non-existant variables. Must be a better way to do
     % this, but it seems expedient and is relatively fast.
     try
         loadedVar = load(thisFullFile, '-mat', 'fd');
-        
+                        
         % Does the file have an FD structure?
         if isfield(loadedVar, 'fd')
+            
+            debugout('Found fd variable')
+            
+            % Handle old combined valve data
+            fixOldValveFile();
             
             % Make the new filename
             newFileName_str = makeFileNameForFD(loadedVar.fd);
@@ -114,21 +141,147 @@ for i = 1:numberOfFiles
         else
             % Doesn't contain fd structure
             % Do nothing to ignore?
+            
+            debugout('No fd variable found')
+            
         end
             
-    catch
+    catch ME
         % Assume unable to load variable 'fd'
         % Could have been errors in file handling.
         % Copy file to error directory for visibility
         copyfile(thisFullFile, errorDir_path);
+        
+        debugout('error in processing data file:');
+        debugout(ME)
     end
     
     totalFrac = ( (2*numberOfFiles) + i)/totalLoops;
     progressbar(totalFrac, [], [], i/numberOfFiles);
     
+    
+    
+
+    
 end
 
 
+
+
+%% Sub Functions:
+% -------------------------------------------------------------------------
+
+function fixOldValveFile
+    % fixOldValveFile makes a temporary fd struct, moves the proportional
+    % data into the normal 'ts' field, clears all old-system fields that
+    % refer to 'isProportional' and 'position', saves the file, and then
+    % updates the original FD to remove the proportional data so it can be
+    % processed as a normal FD variable.
+
+    %     fd = 
+    % 
+    %                 ID: '3055'
+    %               Type: 'PCVNC'
+    %             System: 'LN2'
+    %         FullString: 'LN2 PCVNC-3055 State'
+    %                 ts: [1x1 timeseries]
+    %            isValve: 1
+    %     isProportional: 1
+    %           position: [1x1 timeseries]
+    
+    debugout('Checking for legacy valve data...')
+
+    fd = loadedVar.fd;
+    
+    debugout('Assigning temporary fd variable')
+    debugout(fd)
+    
+    if isfield(fd, 'position')
+        
+        debugout('Found a position timeseries')
+        debugout(fd)
+        
+        % Move the positioner data to the ts field
+        fd.ts = fd.position;
+        
+        % Remove legacy fields from FD struct
+        fd = rmfield(fd, 'isProportional');
+        fd = rmfield(fd, 'position');
+        
+        % Should we remove the isValve tag? Not right now
+        % fd = rmfield(fd, 'isValve');
+        
+        % Need to fix FullString in case the old parser broke it
+        debugout(sprintf('Updating position data file name for %s', fd.ts.Name))
+        
+        fd.ts.Name = getValvePositionFDfromBrokenFDname(fd.ts.Name);
+        
+        % Update fullstring from the new ts
+        fd.FullString = fd.ts.Name;
+        
+        debugout('Created new FD for valve position data')
+        debugout(fd)
+        
+        % Save updated data
+        % -----------------------------------------------------------------
+        
+            % Make the new filename
+            newFileName_str = makeFileNameForFD(fd);
+
+            renamedFullFile = fullfile(rootDir_path, strcat(newFileName_str, '.mat') );
+
+            % Check for Filename Collisions
+            if ~exist(renamedFullFile, 'file')
+
+                % Save the new data file
+                save(renamedFullFile, 'fd');
+                
+                debugout('Saved valve position data to file')
+
+            else
+                % There was a collison
+
+                % put the offending file in issues directory and leave
+                % the original alone. Move on
+                save( fullfile(issuesDir_path, newFileName_str), 'fd');
+                
+                debugout('Filename collision, saving file to issues subdirectory')
+
+            end
+            
+            
+        % Update the original FD to remove the proportional data so it can 
+        % be processed as a normal FD variable
+        % -----------------------------------------------------------------
+        
+        debugout('Removing position data from originally loaded fd variable')
+        
+        % Remove legacy fields from FD struct
+        loadedVar.fd = rmfield(loadedVar.fd, 'isProportional');
+        loadedVar.fd = rmfield(loadedVar.fd, 'position');
+        
+        % Should we remove the isValve tag? Not right now
+        % loadedVar.fd = rmfield(loadedVar.fd, 'isValve');
+        
+        % Update fullstring from the new ts
+        loadedVar.fd.FullString = loadedVar.fd.ts.Name;
+        
+        debugout(loadedVar.fd)
+        
+        % After this point, the loadedVar.fd should be in good shape to be
+        % re-indexed/updated by the main loop. Returning to normal function
+        % business logic
+        
+        debugout('Returning to normal processing loop')
+        
+    end
+    
+    
+
+end
+
+
+end % End of Main Function
 
 
 
